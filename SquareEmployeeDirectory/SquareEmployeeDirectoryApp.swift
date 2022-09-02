9bd289ac620dffa5b22698ce66d685441883d0e6
Alexander Adegbenro <52863002+AlexanderAdegbenro@users.noreply.github.com>
2022-09-02 14:57:51 -0400

    Initial Commit

---
+++ SquareEmployeeDirectory/SquareEmployeeDirectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquareEmployeeDirectoryApp.swift
+//  SquareEmployeeDirectory
+//
+//  Created by Alexander Adgebenro on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct SquareEmployeeDirectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
